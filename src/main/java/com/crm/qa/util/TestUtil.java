292da47ef8bd9e1148ce823b0a92755d216a659c
Gokul2172001 <gokul.elumalai12@gmail.com>
2023-04-14 10:14:37 +0530

    first commit

---
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -0,0 +1,14 @@
+package com.crm.qa.util;
+
+import com.crm.qa.base.TestBase;
+
+public class TestUtil extends TestBase {
+	// Time setting for waiting before working on it.
+	public static long PAGE_LOAD_TIMEOUT =20;
+	public static long PAGE_IMPLICIT_WAIT = 10;
+	
+//	public void switchToFrame() {
+//		driver.switchTo().frame("mainpanel"); 
+//		
+//	}
+}
