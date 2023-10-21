adc1a480ed426c7e5c5b33a93df48686a9b5bebf
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2023-10-21 22:59:55 +0530

    Update TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -3,7 +3,7 @@ package com.crm.qa.util;
 import com.crm.qa.base.TestBase;
 
 public class TestUtil extends TestBase {
-	// Time setting for waiting before working on it.
+	// 	
 	public static long PAGE_LOAD_TIMEOUT =20;
 	public static long PAGE_IMPLICIT_WAIT = 10;
 	
